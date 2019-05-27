1dbc29487fa26620ea4802df42f99985dd6a018a
agrosson <agrosson@gmail.com>
2019-05-27 13:34:39 +0200

    Add food image

---
+++ XellissimeRecipe/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  XellissimeRecipe
+//
+//  Created by ALEXANDRE GROSSON on 27/05/2019.
+//  Copyright © 2019 GROSSON. All rights reserved.
+//
+
+import Foundation
+
+
+// Credit Photo by Brooke Lark on Unsplash
+
+

309fc946df8c1a9076bddca376b68e70f5dc6a81
agrosson <agrosson@gmail.com>
2019-05-28 08:52:01 +0200

    Create file NetworkingClient.swift, Settings.swift

---
+++ XellissimeRecipe/Model/NetworkingClient.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkingClient.swift
+//  XellissimeRecipe
+//
+//  Created by ALEXANDRE GROSSON on 28/05/2019.
+//  Copyright © 2019 GROSSON. All rights reserved.
+//
+
+import Foundation

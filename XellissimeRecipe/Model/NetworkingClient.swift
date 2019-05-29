80e7e120b256f619203cf75600a0a62d91dd7739
agrosson <agrosson@gmail.com>
2019-05-29 13:47:33 +0200

    Refactoring Alamofire request

--- XellissimeRecipe/Model/NetworkingClient.swift
+++ XellissimeRecipe/Model/NetworkingClient.swift
@@ -18,9 +18,9 @@ class NetworkingClient {
     
     // MARK: - Methods
     /**
-    Alamofire request to Edamam API, with list of ingredients in the request
-    - Parameter ingredients: an array of ingredients (String)
-    */
+     Alamofire request to Edamam API, with list of ingredients in the request
+     - Parameter ingredients: an array of ingredients (String)
+     */
     func search(with ingredients: [String]){
         listOfRecipes = [MyRecipe]()
         var items = ""
@@ -32,32 +32,27 @@ class NetworkingClient {
         // Number of response si limited to 20
         let url = "https://api.edamam.com/search?q=\(items)&app_id=\(appId)&app_key=\(appKey)&to=20"
         Alamofire.request(url).responseJSON { (response) in
-            if let myResponse = response.result.value as? [String : Any] {
-                if let hits = myResponse["hits"] as? [[String : Any]] {
-                    print("là")
-                    print(hits.count)
-                    let numberOfResponse = hits.count
-                    for number in 0..<numberOfResponse {
-                        let recipeToShow = MyRecipe(name: "")
-                        if let recipeDetails = hits[number]["recipe"] as? [String : Any] {
-                            if let recipeName = recipeDetails["label"] as? String {
-                                print("Victoire on a le nom de la recette \(recipeName)")
-                                recipeToShow.name = recipeName
-                            }
-                            if let recipeImage = recipeDetails["image"] as? String {
-                                print("le lien de l'image est le suivant : \(recipeImage)")
-                                recipeToShow.urlPhoto = recipeImage
-                            }
-                            if let recipeDetails = recipeDetails["url"] as? String {
-                                print("le lien de la recette est le suivant : \(recipeDetails)")
-                                recipeToShow.urlRecipeDetail = recipeDetails
-                            }
-                        }
-                    }
-                   
-                }
-
+            guard let myResponse = response.result.value as? [String : Any] else {return}
+            guard let hits = myResponse["hits"] as? [[String : Any]] else {return}
+            print("là")
+            print(hits.count)
+            let numberOfResponse = hits.count
+            for number in 0..<numberOfResponse {
+                let recipeToShow = MyRecipe(name: "")
+                guard let recipeDetails = hits[number]["recipe"] as? [String : Any] else {return}
+                guard let recipeName = recipeDetails["label"] as? String else {return}
+                print("Victoire on a le nom de la recette \(recipeName)")
+                recipeToShow.name = recipeName
+                guard let recipeImage = recipeDetails["image"] as? String else {return}
+                print("le lien de l'image est le suivant : \(recipeImage)")
+                recipeToShow.urlPhoto = recipeImage
+                guard let recipePrepare = recipeDetails["url"] as? String else {return}
+                print("le lien de la recette est le suivant : \(recipePrepare)")
+                recipeToShow.urlRecipeDetail = recipePrepare
             }
         }
     }
 }
+
+
+

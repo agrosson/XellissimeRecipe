2d94e137d8a2b6f758851347cb39411a04ca002b
agrosson <agrosson@gmail.com>
2019-05-30 18:37:46 +0200

    Create class CoreRecipe and CoreFavorite

---
+++ XellissimeRecipe/Model/CoreFavorite.swift
@@ -0,0 +1,14 @@
+//
+//  CoreFavorite.swift
+//  XellissimeRecipe
+//
+//  Created by ALEXANDRE GROSSON on 30/05/2019.
+//  Copyright © 2019 GROSSON. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreFavorite: NSManagedObject {
+    
+}

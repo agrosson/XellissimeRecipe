b4d11267fda66f5fede98f7439b0915311800e78
agrosson <agrosson@gmail.com>
2019-05-30 19:21:40 +0200

    Layout for favorite

---
+++ XellissimeRecipe/Controller/FavoriteViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteViewController.swift
+//  XellissimeRecipe
+//
+//  Created by ALEXANDRE GROSSON on 30/05/2019.
+//  Copyright © 2019 GROSSON. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ XellissimeRecipe/Views/FavoriteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteTableViewCell.swift
+//  XellissimeRecipe
+//
+//  Created by ALEXANDRE GROSSON on 30/05/2019.
+//  Copyright © 2019 GROSSON. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

3f6ea83c3a6f9054ece324c17d185fa20cbc6661
agrosson <agrosson@gmail.com>
2019-06-05 10:52:42 +0200

    refactoring code for unit tests

--- XellissimeRecipeTests/FakeDataForAlamoFire.swift
+++ XellissimeRecipeTests/FakeDataForAlamoFire.swift
@@ -12,41 +12,6 @@ import  CoreData
 
 
 class FakeNetworkResponseData {
-    // instance of response OK - statusCode 200
-    static let responseOK = [String:String]()
-    static let responseKO = "It's an error"
-    
-    static let hitsOK = [String:String]()
-    static let hitsKO = "It's an error"
-    
-    static let numberOfResponseKO = 0
-    static let numberOfResponseOK = 3
-    
-    static let recipeToShowOK = MyRecipe(name: "pizza")
-    static let recipeToShowKO = 0
-    
-    
-    static let recipeDetailsOK = [String:String]()
-    static let recipeDetailsKO = "It's an error"
-    
-    
-    static let recipeNameOK = "un string"
-    static let recipeNameK0 = 0
-    
-    static let recipeImageOK = "un string"
-    static let recipeImageKO = 0
-    
-    static let recipePrepareOk = "un string"
-    static let recipePrepareKO = 0
-    
-    static let recipeIngredientsOK = [String]()
-    static let recipeIngredientsKO = 0
-    
-    static let recipeTimeOK = 20
-    static let recipeTimeKO = "bad"
-    
-    static let listOfRecipesOK = [MyRecipe]()
-    static let listOfRecipesKO = "nothing"
     
     static var jsonCorrectDataUrl: String {
         // retrieve bundle where FakeWeatherResponseData is
@@ -62,41 +27,60 @@ class FakeNetworkResponseData {
         let url = bundle.url(forResource: "responseRecipeFakeNoName", withExtension: "json")!
         return url.absoluteString
     }
-}
-/*
-func search(with ingredients: [String], callBack: @escaping (Bool, [MyRecipe]?) -> ()){
-    listOfRecipes = [MyRecipe]()
-    var items = ""
-    for ingredient in ingredients {
-        items += "+\(ingredient)"
+    static var responseRecipeFakeEmptyJson: String {
+        // retrieve bundle where FakeWeatherResponseData is
+        let bundle = Bundle(for: FakeNetworkResponseData.self)
+        // retrieve url of file to test
+        let url = bundle.url(forResource: "responseRecipeEmpty", withExtension: "json")!
+        return url.absoluteString
     }
-    let appId = valueForAPIKey(named: "APIEdamamId")
-    let appKey = valueForAPIKey(named: "APIEdamamKeys")
-    // Number of response si limited to 30
-    let url = "https://api.edamam.com/search?q=\(items)&app_id=\(appId)&app_key=\(appKey)&to=30"
-    Alamofire.request(url).responseJSON { (response) in
-        DispatchQueue.main.async {
-            guard let myResponse = response.result.value as? [String : Any] else { callBack(false, nil);return}
-            guard let hits = myResponse["hits"] as? [[String : Any]] else { callBack(false, nil);return}
-            let numberOfResponse = hits.count
-            for number in 0..<numberOfResponse {
-                guard let recipeToShow = MyRecipe(name: " ") else { callBack(false, nil);return}
-                guard let recipeDetails = hits[number]["recipe"] as? [String : Any] else { callBack(false, nil);return}
-                guard let recipeName = recipeDetails["label"] as? String else { callBack(false, nil);return}
-                recipeToShow.name = recipeName
-                guard let recipeImage = recipeDetails["image"] as? String else { callBack(false, nil);return}
-                recipeToShow.urlPhoto = recipeImage
-                guard let recipePrepare = recipeDetails["url"] as? String else { callBack(false, nil);return}
-                recipeToShow.urlRecipeDetail = recipePrepare
-                guard let recipeIngredients = recipeDetails["ingredientLines"] as? [String] else { callBack(false, nil);return}
-                for individualIngredient in recipeIngredients {
-                    recipeToShow.ingredient.append(individualIngredient)
-                }
-                guard let recipeTime = recipeDetails["totalTime"] as? Int else { callBack(false, nil);return}
-                recipeToShow.cookingTime = recipeTime
-                listOfRecipes.append(recipeToShow)
-            }
-            callBack(true, listOfRecipes);return}
+    static var responseRecipeFakeNoHit: String {
+        // retrieve bundle where FakeWeatherResponseData is
+        let bundle = Bundle(for: FakeNetworkResponseData.self)
+        // retrieve url of file to test
+        let url = bundle.url(forResource: "responseRecipeFakeNoHit", withExtension: "json")!
+        return url.absoluteString
+    }
+    static var responseRecipeFakeNoDetails: String {
+        // retrieve bundle where FakeWeatherResponseData is
+        let bundle = Bundle(for: FakeNetworkResponseData.self)
+        // retrieve url of file to test
+        let url = bundle.url(forResource: "responseRecipeFakeNoDetails", withExtension: "json")!
+        return url.absoluteString
+    }
+    static var responseRecipeFakeNoTime: String {
+        // retrieve bundle where FakeWeatherResponseData is
+        let bundle = Bundle(for: FakeNetworkResponseData.self)
+        // retrieve url of file to test
+        let url = bundle.url(forResource: "responseRecipeFakeNoTime", withExtension: "json")!
+        return url.absoluteString
+    }
+    static var responseRecipeFakeBadFormat: String {
+        // retrieve bundle where FakeWeatherResponseData is
+        let bundle = Bundle(for: FakeNetworkResponseData.self)
+        // retrieve url of file to test
+        let url = bundle.url(forResource: "responseRecipeFakeBadFormat", withExtension: "json")!
+        return url.absoluteString
+    }
+    static var responseRecipeFakeNoUrl: String {
+        // retrieve bundle where FakeWeatherResponseData is
+        let bundle = Bundle(for: FakeNetworkResponseData.self)
+        // retrieve url of file to test
+        let url = bundle.url(forResource: "responseRecipeFakeNoUrl", withExtension: "json")!
+        return url.absoluteString
+    }
+    static var responseRecipeFakeNoIngredientLines: String {
+        // retrieve bundle where FakeWeatherResponseData is
+        let bundle = Bundle(for: FakeNetworkResponseData.self)
+        // retrieve url of file to test
+        let url = bundle.url(forResource: "responseRecipeFakeNoIngredientLines", withExtension: "json")!
+        return url.absoluteString
+    }
+    static var responseRecipeFakeNoImage: String {
+        // retrieve bundle where FakeWeatherResponseData is
+        let bundle = Bundle(for: FakeNetworkResponseData.self)
+        // retrieve url of file to test
+        let url = bundle.url(forResource: "responseRecipeFakeNoImage", withExtension: "json")!
+        return url.absoluteString
     }
 }
-*/
--- XellissimeRecipeTests/NetworkingClientTest.swift
+++ XellissimeRecipeTests/NetworkingClientTest.swift
@@ -11,9 +11,18 @@ import CoreData
 @testable import XellissimeRecipe
 
 class NetworkingClientTest: XCTestCase {
+    
+    // Test create url
+    func testCreateURLAsAStringFromArrayOfString(){
+        let ingredients = ["cheese", "tomato"]
+        let result = NetworkingClient.shared.createURL(with: ingredients)
+        let stringExpected = "https://api.edamam.com/search?q=+cheese+tomato&app_id=7ba6f788&app_key=58fb0a71d4b3042d003fa99123a86f75&to=30"
+        XCTAssertEqual(result, stringExpected)
+    }
 
+
+    // Test correct json file
     func testAlamofireOK(){
-       // let expectation = self.expectation(description: "TestAlamo")
         let expectation = self.expectation(description: "Alamo")
         let url = FakeNetworkResponseData.jsonCorrectDataUrl
         NetworkingClient.shared.search(with: url) { (success, recipes) in
@@ -21,11 +30,10 @@ class NetworkingClientTest: XCTestCase {
             XCTAssertNotNil(recipes)
             expectation.fulfill()
         }
-        wait(for: [expectation], timeout: 5)
+        wait(for: [expectation], timeout: 2)
     }
-    
+    // Test Json file with no recipe name
     func testAlamofireKONoName(){
-        // let expectation = self.expectation(description: "TestAlamo")
         let expectation = self.expectation(description: "Alamo")
         let url = FakeNetworkResponseData.responseRecipeFakeNoName
         NetworkingClient.shared.search(with: url) { (success, recipes) in
@@ -33,11 +41,88 @@ class NetworkingClientTest: XCTestCase {
             XCTAssertNil(recipes)
             expectation.fulfill()
         }
-        wait(for: [expectation], timeout: 5)
+        wait(for: [expectation], timeout: 2)
     }
-    
-    
-    
-    
+    // test empty Json File
+    func testAlamofireFakeEmptyJsonFile(){
+        let expectation = self.expectation(description: "Alamo")
+        let url = FakeNetworkResponseData.responseRecipeFakeEmptyJson
+        NetworkingClient.shared.search(with: url) { (success, recipes) in
+            XCTAssertFalse(success)
+            XCTAssertNil(recipes)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    func testAlamofireFakeNoHit(){
+        let expectation = self.expectation(description: "Alamo")
+        let url = FakeNetworkResponseData.responseRecipeFakeNoHit
+        NetworkingClient.shared.search(with: url) { (success, recipes) in
+            XCTAssertFalse(success)
+            XCTAssertNil(recipes)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    func testAlamofireFakeNoDetails(){
+        let expectation = self.expectation(description: "Alamo")
+        let url = FakeNetworkResponseData.responseRecipeFakeNoDetails
+        NetworkingClient.shared.search(with: url) { (success, recipes) in
+            XCTAssertFalse(success)
+            XCTAssertNil(recipes)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    func testAlamofireFakeBadFormat(){
+        let expectation = self.expectation(description: "Alamo")
+        let url = FakeNetworkResponseData.responseRecipeFakeBadFormat
+        NetworkingClient.shared.search(with: url) { (success, recipes) in
+            XCTAssertFalse(success)
+            XCTAssertNil(recipes)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    func testAlamofireFakeNoUrl(){
+        let expectation = self.expectation(description: "Alamo")
+        let url = FakeNetworkResponseData.responseRecipeFakeNoUrl
+        NetworkingClient.shared.search(with: url) { (success, recipes) in
+            XCTAssertFalse(success)
+            XCTAssertNil(recipes)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    func testAlamofireFakeNoTime(){
+        let expectation = self.expectation(description: "Alamo")
+        let url = FakeNetworkResponseData.responseRecipeFakeNoTime
+        NetworkingClient.shared.search(with: url) { (success, recipes) in
+            XCTAssertFalse(success)
+            XCTAssertNil(recipes)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    func testAlamofireFakeNoIngredientLines(){
+        let expectation = self.expectation(description: "Alamo")
+        let url = FakeNetworkResponseData.responseRecipeFakeNoIngredientLines
+        NetworkingClient.shared.search(with: url) { (success, recipes) in
+            XCTAssertFalse(success)
+            XCTAssertNil(recipes)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    func testAlamofireFakeNoImage(){
+        let expectation = self.expectation(description: "Alamo")
+        let url = FakeNetworkResponseData.responseRecipeFakeNoImage
+        NetworkingClient.shared.search(with: url) { (success, recipes) in
+            XCTAssertFalse(success)
+            XCTAssertNil(recipes)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2)
+    }
     
 }

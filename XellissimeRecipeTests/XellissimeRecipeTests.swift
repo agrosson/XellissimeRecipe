a39fd28774936455ba3b3924e0f7444c7434250f
agrosson <agrosson@gmail.com>
2019-06-10 17:57:25 +0200

    Final commit tests

--- XellissimeRecipeTests/XellissimeRecipeTests.swift
+++ XellissimeRecipeTests/XellissimeRecipeTests.swift
@@ -14,24 +14,34 @@ class XellissimeRecipeTests: XCTestCase {
 
     // Test if can create an instance of CoreRecipe
     func testInitCoreRecipe() {
+        let testCountBaseInit = CoreRecipe.all.count
         let instance = CoreRecipe()
         XCTAssertNotNil(instance)
+        let testCountBaseFinal = CoreRecipe.all.count
+        XCTAssertEqual(testCountBaseInit, testCountBaseFinal)
     }
     
     // Test if container exists
     func testIfContainerExists() {
+        let testCountBaseInit = CoreRecipe.all.count
         let instance = AppDelegate.persistentContainer
         XCTAssertNotNil(instance)
+        let testCountBaseFinal = CoreRecipe.all.count
+        XCTAssertEqual(testCountBaseInit, testCountBaseFinal)
     }
     
     // Test if context exists
     func testIfContextExists() {
+        let testCountBaseInit = CoreRecipe.all.count
         let instance = AppDelegate.viewContext
         XCTAssertNotNil(instance)
+        let testCountBaseFinal = CoreRecipe.all.count
+        XCTAssertEqual(testCountBaseInit, testCountBaseFinal)
     }
     
     // Test to create a specific CoreRecipe object
     func testCreateACoreRecipeObjetc(){
+        let testCountBaseInit = CoreRecipe.all.count
         let object = CoreRecipe(context: AppDelegate.viewContext)
         object.name = "Pizza"
         object.ingredient = ["tomato","olive","bread"]
@@ -39,81 +49,105 @@ class XellissimeRecipeTests: XCTestCase {
         object.urlPhoto = nil
         object.urlRecipeDetails = "http:Miam"
         XCTAssertNotNil(object)
+        CoreRecipe.removeFromFavorite(coreRecipe: object)
+        let testCountBaseFinal = CoreRecipe.all.count
+        XCTAssertEqual(testCountBaseInit, testCountBaseFinal)
     }
     
     
     // Test to save a recipe into Favorite
     func testToSaveARecipeAsAFavorite(){
+        let testCountBaseInit = CoreRecipe.all.count
         // Given
         let numberBefore = CoreRecipe.all.count
         // and
-        guard let recipe = MyRecipe(name: "Pizza") else {return}
+        let uuid = UUID()
+        guard let recipe = MyRecipe(name: "\(uuid)") else {return}
         // When save recipe
         CoreRecipe.saveToFavorite(recipe: recipe)
         // Then favorite number increases of 1 item
         let numberAfter = CoreRecipe.all.count
         XCTAssertEqual(numberAfter, numberBefore+1)
-        guard let coreLast = CoreRecipe.all.last else {return}
-        CoreRecipe.removeFromFavorite(coreRecipe: coreLast)
+        // remove after test
+        CoreRecipe.removeACoreRecipeFromMyRecipe(recipe: recipe)
+        let testCountBaseFinal = CoreRecipe.all.count
+        XCTAssertEqual(testCountBaseInit, testCountBaseFinal)
     }
     
     // Test if Duplicate favorite in database
     func testIfDuplicate(){
+        let testCountBaseInit = CoreRecipe.all.count
         //given that a recipe with name Pizza has been saved
-        guard let recipe = MyRecipe(name: "Pizza") else {return}
+        let uuid = UUID()
+        guard let recipe = MyRecipe(name: "\(uuid)") else {return}
         recipe.urlRecipeDetail = "urlTest"
         CoreRecipe.saveToFavorite(recipe: recipe)
         // when try to save same item
-        guard let recipe2 = MyRecipe(name: "Pizza") else {return}
+        guard let recipe2 = MyRecipe(name: "\(uuid)") else {return}
         // the testIsTrue
         recipe2.urlRecipeDetail = "urlTest"
         let test = CoreRecipe.checkIfRecipeIsAlreadyInFavorite(recipe: recipe2)
         XCTAssertTrue(test)
-        guard let coreLast = CoreRecipe.all.last else {return}
-        CoreRecipe.removeFromFavorite(coreRecipe: coreLast)
+        CoreRecipe.removeACoreRecipeFromMyRecipe(recipe: recipe)
+        let testCountBaseFinal = CoreRecipe.all.count
+        XCTAssertEqual(testCountBaseInit, testCountBaseFinal)
     }
     
     // Test if Duplicate favorite in database
     func testIfDuplicateNoBecauseOfURL(){
+        let testCountBaseInit = CoreRecipe.all.count
         //given that a recipe with name Pizza has been saved
-        guard let recipe = MyRecipe(name: "Pizza") else {return}
+        let uuid = UUID()
+        guard let recipe = MyRecipe(name: "\(uuid)") else {return}
         recipe.urlRecipeDetail = "urlTest"
         CoreRecipe.saveToFavorite(recipe: recipe)
         // when try to save same item
-        guard let recipe2 = MyRecipe(name: "Pizza") else {return}
+        guard let recipe2 = MyRecipe(name: "\(uuid)") else {return}
         // the testIsTrue
         recipe2.urlRecipeDetail = "urlTest&"
         let test = CoreRecipe.checkIfRecipeIsAlreadyInFavorite(recipe: recipe2)
         XCTAssertFalse(test)
+        CoreRecipe.removeACoreRecipeFromMyRecipe(recipe: recipe)
+        let testCountBaseFinal = CoreRecipe.all.count
+        XCTAssertEqual(testCountBaseInit, testCountBaseFinal)
     }
     
     
     // Test if non Duplicate favorite in database
     func testIfNoDuplicate(){
+        let testCountBaseInit = CoreRecipe.all.count
         //given that a recipe with unique name
         let uuid = UUID()
         guard let recipe = MyRecipe(name: "\(uuid)") else {return}
         // the testIsTrue
         let test = CoreRecipe.checkIfRecipeIsAlreadyInFavorite(recipe: recipe)
         XCTAssertFalse(test)
-        guard let coreLast = CoreRecipe.all.last else {return}
-        CoreRecipe.removeFromFavorite(coreRecipe: coreLast)
+        let testCountBaseFinal = CoreRecipe.all.count
+        XCTAssertEqual(testCountBaseInit, testCountBaseFinal)
     }
     
     // test function remove
     func testRemoveItemFromFavorite(){
+        let testCountBaseInit = CoreRecipe.all.count
+        let uuid = UUID()
+        guard let recipe = MyRecipe(name: "\(uuid)") else {return}
+        recipe.urlRecipeDetail = "urlTest"
+        CoreRecipe.saveToFavorite(recipe: recipe)
         // Given a item in database : firstOne
-        guard let itemToDelete = CoreRecipe.all.first else {return}
+        guard let itemToDelete = CoreRecipe.all.last else {return}
         let numberBefore = CoreRecipe.all.count
         // When delete an item
         CoreRecipe.removeFromFavorite(coreRecipe: itemToDelete)
         // then the number of item in database is -1
         let numberAfter = CoreRecipe.all.count
         XCTAssertEqual(numberAfter, numberBefore-1)
+        let testCountBaseFinal = CoreRecipe.all.count
+        XCTAssertEqual(testCountBaseInit, testCountBaseFinal)
     }
     
     // Test function remove a favorite CoreRecipe from a MyRecipe diplayed
     func testGivenAMyRecipeWhenAlreadyInFavoriteThenRemoveWhenClicOnFavoriteIcon() {
+        let testCountBaseInit = CoreRecipe.all.count
         let uuid = UUID()
         let uuidURl = UUID()
         guard let recipeToBeLast = MyRecipe(name: "\(uuid)") else {return}
@@ -128,8 +162,8 @@ class XellissimeRecipeTests: XCTestCase {
         CoreRecipe.removeACoreRecipeFromMyRecipe(recipe: itemMyRecipe)
         let numberAfter = CoreRecipe.all.count
         XCTAssertEqual(numberAfter, numberFavoriteBefore-1)
-        guard let coreLast = CoreRecipe.all.last else {return}
-        CoreRecipe.removeFromFavorite(coreRecipe: coreLast)
+         let testCountBaseFinal = CoreRecipe.all.count
+        XCTAssertEqual(testCountBaseInit, testCountBaseFinal)
     }
 }
 

98be0babaab151a23dedadaba7c1957bd69dfb26
agrosson <agrosson@gmail.com>
2019-06-10 10:37:24 +0200

    Documentation of RecipeListTableViewCell

--- XellissimeRecipe/Views/RecipeListTableViewCell.swift
+++ XellissimeRecipe/Views/RecipeListTableViewCell.swift
@@ -8,24 +8,31 @@
 
 import UIKit
 
+// MARK: - Class RecipeListTableViewCell
+/**
+ This class defines the characteristics of the cell
+ 
+ */
 class RecipeListTableViewCell: UITableViewCell {
     @IBOutlet weak var imageRecipe: UIImageView!
     @IBOutlet weak var nameRecipe: UILabel!
     @IBOutlet weak var timeRecipe: UILabel!
     @IBOutlet weak var ingredientsRecipe: UILabel!
     
-    
     override func awakeFromNib() {
         super.awakeFromNib()
-       
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    /**
+     This function enables the configuration of the cell
+     - Parameter withImage: the url as a string of the image
+     - Parameter name: the title of the recipe
+     - Parameter time: the cooking time of the recipe
+     - Parameter ingredients: the list as an array of ingredients of the recipe
+     */
     func configure(withImage: String, name: String, time: Int, ingredients: [String]) {
         var imageToGet : UIImage?
         guard let url = URL(string: withImage ) else {return}
